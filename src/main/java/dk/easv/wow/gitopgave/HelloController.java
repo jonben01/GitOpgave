a987ae379aaee1fcefa75baa7330c506b15ae9de
Vladyslav <vlazav01@easv365.dk>
2024-11-14 10:20:06 +0100

    tilføjet something

--- src/main/java/dk/easv/wow/gitopgave/HelloController.java
+++ src/main/java/dk/easv/wow/gitopgave/HelloController.java
@@ -4,8 +4,20 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
+    private Label getWelcomeText() {
+        return welcomeText;
+    }
+
+    private void setWelcomeText(Label welcomeText) {
+        this.welcomeText = welcomeText;
+    }
+
     @FXML
     private Label welcomeText;
+
+    private HelloController(Label welcomeText) {
+        this.welcomeText = welcomeText;
+    }
 
     @FXML
     protected void onHelloButtonClick() {
