aa9af7905361eaa2a5434d756f6d5669ef774275
thorjessing <thorwinddk@gmail.com>
2024-11-14 10:45:35 +0100

    hej

--- src/main/java/dk/easv/wow/gitopgave/HelloApplication.java
+++ src/main/java/dk/easv/wow/gitopgave/HelloApplication.java
@@ -7,6 +7,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 //thing
+// hej
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
